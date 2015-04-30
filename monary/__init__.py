9320a9abd1aed74f8b08c3db339e5ab8030d5eb8
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2015-04-30 22:14:37 +0000

    Update version to 0.4.0 in __init__.py

--- monary/__init__.py
+++ monary/__init__.py
@@ -7,5 +7,5 @@ from .write_concern import (WriteConcern, MONARY_W_ERRORS_IGNORED,
 from .monary_param import MonaryParam
 from .datehelper import mongodate_to_datetime
 
-version = "0.3.0"
+version = "0.4.0"
 __version__ = version
